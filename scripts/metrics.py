d32f11073b0b8fa276de172aa502f753837f176f
jarrjam <jamil.daher1@hotmail.com>
2020-10-31 21:04:29 +1100

    Added mse/mae evaluations for grades with at least 30 samples

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -40,11 +40,21 @@ def ordinal_evaluation_report(y_true, y_pred, labels=None):
         labels = unique
 
     class_scores = {label: {'mse': 0, 'mae': 0} for label in labels}
+
     total_mse = 0
     total_mae = 0
 
+    # These scores are based off grades with at least 100 samples in the test dataset
+    subset_mse = 0
+    subset_mae = 0
+    subset_support = 0
+    subset_grades = {}
+
     for i in range(len(y_true)):
         curr_label = y_true[i]
+
+        if counts[curr_label - 4] >= 30:
+            subset_grades[curr_label] = True
 
         if curr_label in class_scores:
             prediction_error = abs(curr_label-y_pred[i])
@@ -56,6 +66,11 @@ def ordinal_evaluation_report(y_true, y_pred, labels=None):
 
             class_scores[curr_label]['mse'] += mse
             total_mse += mse
+
+            if curr_label in subset_grades:
+                subset_support += 1
+                subset_mse += mse
+                subset_mae += mae
 
     headers = ['Class', 'MSE', 'MAE', 'Support']
     data = [headers] + \
@@ -71,8 +86,14 @@ def ordinal_evaluation_report(y_true, y_pred, labels=None):
         if i in [0, len(data) - 1]:
             output_string += '-' * len(line) + '\n'
 
+    output_string += '|'.join(str(item).ljust(8) for item in ['Subset', "{0:.5g}".format(
+        subset_mse/len(subset_grades)), "{0:.5g}".format(subset_mae/len(subset_grades)), subset_support])
+    output_string += '\n'
+
     output_string += '|'.join(str(item).ljust(8) for item in ['Total', "{0:.5g}".format(total_mse/len(
         unique)), "{0:.5g}".format(total_mae/len(unique)), len(y_true)])
     output_string += '\n'
+
+    output_string += '\nSubset scores are based on grades with at least 30 samples in the test dataset.\n'
 
     return output_string
