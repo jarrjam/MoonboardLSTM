bb7eb6645320cd20afa83c4800de61e84b9b0932
jarrjam <jamil.daher1@hotmail.com>
2020-11-03 17:59:48 +1100

    Changed format of hold keys

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 from scripts.preprocess import *
 from scripts.run_models import *
 
-with open('data/hold_positions.json')as hold_file, open('data/moonboard_problems.json') as problems_file:
+with open('data/hold_positions.json') as hold_file, open('data/moonboard_problems.json') as problems_file:
     hold_positions = json.load(hold_file)
     problems = json.load(problems_file)
 
